6beffcc1b764cf3224c9a12bc550dded9d78bb7c
heyitaki <akshath@berkeley.edu>
2020-05-22 14:54:45 -0700

    separate chat and message

--- src/collections/Chat.ts
+++ src/collections/Chat.ts
@@ -1,10 +1,4 @@
-export default interface Message {
-  id: string;
-  content?: string;
-  creationDatetime?: number;
-  userId?: string;
-  username?: string;
-}
+import Message from "./Message";
 
 export default interface Chat {
   id: string;
---
+++ src/collections/Message.ts
@@ -0,0 +1,7 @@
+export default interface Message {
+  id: string;
+  content?: string;
+  creationDatetime?: number;
+  userId?: string;
+  username?: string;
+}
