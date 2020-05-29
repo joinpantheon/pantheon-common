e1e8d940b01fe89ea82ab2d646dbb492936baaeb
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-05-29 08:50:30 -0700

    Revert "removed subjectstats as that is really not an enum but instead a frontend dictionary to translate subject in the database to the statistic title in another collection"
    This reverts commit 7e399157d627d777b3eba51194dcf6cb7e93acf2.

---
+++ src/enums/SubjectStats.ts
@@ -0,0 +1,14 @@
+enum SubjectStats {
+  Biology = 'biology',
+  Chemistry = 'chemistry',
+  Computer = 'computer',
+  Earth = 'earth',
+  Energy = 'energy',
+  General = 'generalKnowledge',
+  Math = 'math',
+  Physics = 'physics',
+  Space = 'space',
+  AllSubjects = 'allSubjects'
+}
+
+export default SubjectStats;
