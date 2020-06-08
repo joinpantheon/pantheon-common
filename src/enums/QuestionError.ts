4d0adcab03a96121e0c5d9d6308bdaeac6eb185c
heyitaki <akshath@berkeley.edu>
2020-06-08 10:57:04 -0700

    make questionerror string enum

--- src/enums/QuestionError.ts
+++ src/enums/QuestionError.ts
@@ -1,8 +1,12 @@
 enum QuestionError {
-  MALFORMED_MC_CHOICE, // Doubled up choices in MC question
-  DOUBLED_QUESTION, // Doubled up questions in same document
-  MC_ANSWER_MISFORMATTED, // MC answer doesn't start with [w-z])
-  SHORT_ANSWER_NOT_SHORT, // Question requiring 1 sentence+ answer
+  // Doubled up choices in MC question
+  MalformedMcChoices = "Malformed MC choices",
+  // Doubled up questions in same document
+  DoubledQuestion = "Doubled question",
+  // MC answer doesn't start with [w-z])
+  McAnswerMisformatted = "MC answer misformatted",
+  // Question requiring 1+ sentence answer
+  ShortAnswerNotShort = "Short answer not short",
 }
 
 export default QuestionError;
