c8010b77c1ac246852ca6820f1820c9ead31815b
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-06-16 12:33:11 -0700

    moved message inside chat because it's not a collection

--- src/collections/Chat.ts
+++ src/collections/Chat.ts
@@ -1,4 +1,13 @@
-import Message from "./Message";
+import MessageType from '../enums/MessageType';
+
+export interface Message {
+  id: string;
+  content?: string;
+  creationDatetime?: number;
+  userId?: string;
+  username?: string;
+  messageType?: MessageType;
+}
 
 export default interface Chat {
   id: string;
