c1dddc5f3ea8d018a1ea598710409dd1af589b0d
heyitaki <akshath@berkeley.edu>
2020-06-16 17:34:19 -0700

    remove deprecated

--- src/deprecated/Answer.ts
+++
@@ -1,9 +0,0 @@
-import QuestionSubject from "../enums/QuestionSubject";
-
-export default interface Answer {
-  id: string;
-  answerText?: string;
-  questionId?: string;
-  responseType?: string;
-  subject?: QuestionSubject;
-}
