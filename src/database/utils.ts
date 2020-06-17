693165a31b41025d839f06c5ca9347bd0b01f526
heyitaki <akshath@berkeley.edu>
2020-06-17 02:56:35 -0700

    rewrite type coercion with signature overloads

--- src/database/utils.ts
+++ src/database/utils.ts
@@ -5,8 +5,10 @@ import { toArray } from "../utils/general";
 import Database from "./Database";
 import DbGetOpts from "./DbGetOpts";
 
-type Firestore = firebase.firestore.Firestore;
-type Query = firebase.firestore.Query<firebase.firestore.DocumentData>;
+type AdminDb = FirebaseFirestore.Firestore;
+type ClientDb = firebase.firestore.Firestore;
+type Query = FirebaseFirestore.Query<FirebaseFirestore.DocumentData> | 
+  firebase.firestore.Query<firebase.firestore.DocumentData>;
 
 /**
  * Write data to given collection in db. 
@@ -14,13 +16,13 @@ type Query = firebase.firestore.Query<firebase.firestore.DocumentData>;
  * @param collection Name of collection to write to
  * @param doc Document data to write to db
  */
-export function dbWrite(db: Database, collection: CollectionName, 
-    doc: Document): Promise<void> {
-  return (db as Firestore)
+export function dbWrite(db: AdminDb,  collection: CollectionName, doc: Document): Promise<FirebaseFirestore.WriteResult>;
+export function dbWrite(db: ClientDb, collection: CollectionName, doc: Document): Promise<void>;
+export function dbWrite(db: Database, collection: CollectionName, doc: Document) {
+  return db
     .collection(collection)
     .doc(doc.id)
-    .set(doc)
-    .then(() => {});
+    .set(doc);
 }
 
 /**
@@ -29,13 +31,13 @@ export function dbWrite(db: Database, collection: CollectionName,
  * @param collection Name of collection to update
  * @param doc Document data to update existing doc with
  */
-export function dbUpdate(db: Database, collection: CollectionName, 
-    doc: Document): Promise<void> {
-  return (db as Firestore)
+export function dbUpdate(db: AdminDb,  collection: CollectionName, doc: Document): Promise<FirebaseFirestore.WriteResult>;
+export function dbUpdate(db: ClientDb, collection: CollectionName, doc: Document): Promise<void>;
+export function dbUpdate(db: Database, collection: CollectionName, doc: Document) {
+  return db
     .collection(collection)
     .doc(doc.id)
-    .update(doc)
-    .then(() => {});
+    .update(doc);
 }
 
 /**
@@ -44,8 +46,10 @@ export function dbUpdate(db: Database, collection: CollectionName,
  * @param collection Name of collection to get data from
  * @param docId Id of document to retrieve
  */
+export function dbGet(db: AdminDb,  collection: CollectionName, docId: string): Promise<FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData>>;
+export function dbGet(db: ClientDb, collection: CollectionName, docId: string): Promise<firebase.firestore.DocumentSnapshot<firebase.firestore.DocumentData>>;
 export function dbGet(db: Database, collection: CollectionName, docId: string) {
-  return (db as Firestore)
+  return db
     .collection(collection)
     .doc(docId)
     .get();
@@ -56,11 +60,12 @@ export function dbGet(db: Database, collection: CollectionName, docId: string) {
  * @param db Database to get gata from
  * @param collection Name of collection to get data from
  * @param opts Object containing possible filters for get query
- * TODO: check filters to see if they are valid (https://firebase.google.com/docs/firestore/query-data/order-limit-data)
+ * @TODO: check filters to see if they are valid (https://firebase.google.com/docs/firestore/query-data/order-limit-data)
  */
-export function dbGetMultiple(db: Database, collection: CollectionName, 
-    opts: DbGetOpts={}) {
-  const ref = (db as Firestore).collection(collection);
+export function dbGetMultiple(db: AdminDb,  collection: CollectionName, opts?: DbGetOpts): Promise<FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>>
+export function dbGetMultiple(db: ClientDb, collection: CollectionName, opts?: DbGetOpts): Promise<firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>>
+export function dbGetMultiple(db: Database, collection: CollectionName, opts: DbGetOpts={}) {
+  const ref = db.collection(collection);
   let query: Query = ref;
 
   // Handle where
@@ -86,18 +91,4 @@ export function dbGetMultiple(db: Database, collection: CollectionName,
 
   // Return list of documents that satisfy the above filters, if any
   return query.get();
-}
-
-/**
- * Copy collection from one db to another.
- * @param db1 Database to copy data from
- * @param db2 Database to copy data to
- * @param collection Collection name
- */
-export async function dbCopyCollection(db1: Database, db2: Database, 
-    collection: CollectionName) {
-  const snapshot = await dbGetMultiple(db1, collection);
-  const writes = snapshot.docs.map(doc => 
-    dbWrite(db2, collection, doc.data() as Document));
-  return Promise.all(writes);
 }
