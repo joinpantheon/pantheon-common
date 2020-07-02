d521604ea954017e0d24eb81913d18bcc3df74e8
heyitaki <akshath@berkeley.edu>
2020-07-01 18:05:51 -0700

    deleting constants bc it is frontend-specific

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_RATING: number = 1000; // Starting rating
-
-export const QUESTION_DELAY_MS: number = 5 * 1000; // How long before displaying question: 5s
-export const QUESTION_TIMEOUT_MS: number = 30 * 1000; // How long before question times out: 30s
