bc53451db40d0b6b73bb9f9da2f5027451ed52e0
Ali Ahmed <ali.ahmed@berkeley.edu>
2020-07-21 16:16:31 -0700

    bugfix for lastChanged in Status - needs to be Object

--- src/collections/Status.ts
+++ src/collections/Status.ts
@@ -1,9 +1,8 @@
-import { ServerValue } from '@firebase/database-types';
 import { FieldValue } from '@firebase/firestore-types';
 import State from '../enums/State';
 
 export default interface Status {
-  lastChanged: FieldValue | ServerValue;
+  lastChanged: FieldValue | Object;
   state: State;
   userId: string;
   username: string;
