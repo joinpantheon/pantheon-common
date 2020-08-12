d8bc1118002e54c8966bb26ddc0e69651645dc9a
heyitaki <akshath@berkeley.edu>
2020-08-12 09:30:29 -0700

    add id

--- src/collections/Status.ts
+++ src/collections/Status.ts
@@ -2,6 +2,7 @@ import { FieldValue } from '@firebase/firestore-types';
 import State from '../enums/State';
 
 export default interface Status {
+  id: string;
   lastChanged: FieldValue | Object;
   state: State;
   userId: string;
